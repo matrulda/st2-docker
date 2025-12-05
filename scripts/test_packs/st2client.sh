6fe71424122fad4542b5287a79e1110e5e1f7b97
matrulda <matilda.aslin@medsci.uu.se>
2025-12-05 13:44:46 +0100

    Mount test_packs scripts and fix typo

--- scripts/test_packs/st2client.sh
+++ scripts/test_packs/st2client.sh
@@ -10,7 +10,7 @@ ln -fns /opt/stackstorm/packs.dev /opt/stackstorm/packs/test_packs
 st2ctl reload
 
 ## Load snpseq_packs packs
-st2 run packs.load packs=snpseq_packs register=all
+st2 run packs.load packs=test_packs register=all
 
 ## Make virtualenv for snpseq_packs
 st2 run packs.setup_virtualenv packs=test_packs
